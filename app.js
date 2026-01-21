3f0f23da610aec2392286ef9e54b796e2a4a3bbe
Vahe Sevachyan <vscode13@gmail.com>
2026-01-20 17:06:02 -0800

    updated getRecipeData Function
    getRecipeData will be handling the recipe data request in a separate function and open a new page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { MEAL_API_URL } from "./config.js";
 const getDataBtn = document.querySelector("#get-data-btn");
 const inputField = document.querySelector("#inputField");
 const container = document.getElementById("data-container");
+const getRecipeBtn = document.querySelector(".recipe-btn");
 
 getDataBtn.addEventListener("click", () => {
   const searchResult = inputField.value;
@@ -25,12 +26,24 @@ getDataBtn.addEventListener("click", () => {
 function displayData(data) {
   container.innerHTML = "";
   data.meals.forEach((item) => {
+    const getRecipeBtn = document.createElement("button");
+    getRecipeBtn.classList.add("recipe-btn");
+    getRecipeBtn.innerHTML = "Get Recipe";
     const mealCard = document.createElement("div");
+    mealCard.classList.add("meal-card");
     mealCard.innerHTML = `
-      <img src="${item.strMealThumb}" class="thumbnail">
-       <h3> ${item.strMeal}</h3>
-       <button>More info</button>
+      <img src="${item.strMealThumb}" class="meal-img">
+       <span class='meal-name'> ${item.strMeal}</span>
+       <span class='category-name'>Category: ${item.strCategory}</span>
+       <span class='area-name'>Origin: ${item.strArea}</span>
       `;
+    getRecipeBtn.addEventListener("click", () => {
+      getRecipeData();
+    });
+    mealCard.appendChild(getRecipeBtn);
     container.appendChild(mealCard);
   });
+}
+function getRecipeData() {
+  console.log("clicked");
 }
